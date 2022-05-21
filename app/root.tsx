71e00973a2b7f95dca967c515a3b02d3e0ddf3e5
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2022-05-22 01:40:30 +0200

    Add 404 error page

--- app/root.tsx
+++ app/root.tsx
@@ -6,10 +6,13 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from '@remix-run/react'
 import ogLogo from '~/images/og-logo.jpg'
 import styles from './styles/app.css'
 import { MetronomeLinks } from '@metronome-sh/react'
+import { H1 } from './features/Heading'
+import logoInflames from '~/images/logo-inflames.svg'
 
 export const meta: MetaFunction = () => ({
   title: 'IT Begins',
@@ -45,6 +48,48 @@ export function links() {
     { rel: 'manifest', href: '/site.webmanifest' },
     { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
   ]
+}
+
+/**
+ * Catches HTTP errors
+ */
+export function CatchBoundary() {
+  let caught = useCatch()
+  let title = 'Something went wrong'
+  let message
+  switch (caught.status) {
+    case 404:
+      title = 'Not Found'
+      message = 'This page does not exist'
+      break
+
+    default:
+      throw new Error(caught.data || caught.statusText)
+  }
+
+  return (
+    <html lang='en' className='dark'>
+      <head>
+        <title>💔 | IT Begins</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width,initial-scale=1' />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff' />
+        <Links />
+      </head>
+      <body className='transition-color h-full min-h-screen overflow-x-hidden bg-slate-100 text-slate-500 antialiased duration-300 ease-in-out dark:bg-slate-900 dark:text-slate-400'>
+        <div className='flex min-h-screen flex-col items-center justify-center'>
+          <header className='flex flex-col items-center'>
+            <H1>
+              {caught.status}: {title}
+            </H1>
+            <p className='text-2xl'>{message}</p>
+          </header>
+          <img className='mt-4' src={logoInflames} alt='Logo burning' />
+        </div>
+      </body>
+    </html>
+  )
 }
 
 export default function App() {
