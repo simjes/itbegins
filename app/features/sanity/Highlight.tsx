ee30d533769cc96b27a744780956a7bbf0f51b57
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2022-07-02 22:43:23 +0200

    Fix Highlight breaking when going to new line

--- app/features/sanity/Highlight.tsx
+++ app/features/sanity/Highlight.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react'
 import React from 'react'
 
 export const Highlight = ({ children }: { children: ReactNode }) => (
-  <span className='relative ml-2 before:absolute before:-inset-1 before:block before:-skew-y-3 before:bg-gradient-to-r before:from-violet-500 before:to-fuchsia-500'>
-    <span className='relative skew-y-3 text-white'>{children}</span>
+  <span className='inline-block -skew-y-2 transform bg-gradient-to-r from-violet-500 to-fuchsia-500 px-1'>
+    <span className='inline-block skew-y-2 text-white'>{children}</span>
   </span>
 )
