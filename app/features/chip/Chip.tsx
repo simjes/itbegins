4b0e0c7a542fbc0f74f1ec78c4438b749fc1e00a
Simon Jespersen <6494049+simjes@users.noreply.github.com>
2022-07-12 18:38:45 +0200

    Remove job and role tag

--- app/features/chip/Chip.tsx
+++ app/features/chip/Chip.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 
 const Chip = ({ text }: Props) => (
-  <div className='rounded-lg border border-cyan-800 px-2 py-1 font-medium text-cyan-800 dark:border-cyan-400 dark:text-white'>
+  <div className='rounded-lg border-2 border-cyan-500 px-2 py-1 font-medium text-cyan-700 dark:border-cyan-400 dark:text-white'>
     {text}
   </div>
 )
